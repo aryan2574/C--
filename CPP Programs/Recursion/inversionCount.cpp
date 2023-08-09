4c3068bab50c676e3b5d0d7f8011365123135cfa
aryan2574 <ayush.aryan94@yahoo.com>
2023-08-10 00:54:27 +0530

    inversion count

---
+++ CPP Programs/Recursion/inversionCount.cpp
@@ -0,0 +1,154 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1000000007
+#define mod1 998244353
+#define INF 1e18
+#define PI 3.141592653589793238462
+#define nline "\n"
+
+#define sz(x) ((int)(x).size())
+#define set_bits __builtin_popcount
+#define set_bitsll __builtin_popcountll
+#define setp(x, y) fixed << setprecision(y) << x
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#define scnfi(n) scanf("%d", &n)
+#define scnfii(n, m) scanf("%d%d", &n, &m)
+#define scnfl(n) scanf("%I64d", &n)
+#define scnfll(n, m) scanf("%lld%lld", &n, &m)
+#define scnfstr(str) scanf("%s", &str);
+
+#define prntfi(n) printf("%d\n", n)
+#define prntfii(n, m) printf("%d %d\n", n, m)
+#define prntfl(n) printf("%I64d\n", n)
+#define prntfll(n, m) printf("%I64d %I64d\n", n, m)
+#define prntfstr(s) printf("%s\n", s)
+
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+void _print(ll t)
+{
+    cerr << t;
+}
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+void ayush_2574_cp()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
+}
+
+// ____________________________________________________________________________________
+
+// IP : 6
+//      1 5 2 6 3 0
+// OP : 8
+
+int mergeFunc(int *arr, int s, int e)
+{
+    int temp[100];
+    int mid = s + (e - s) / 2;
+
+    int i = s, j = mid + 1, k = s, ans(0);
+
+    while (i <= mid && j <= e)
+    {
+        if (arr[i] <= arr[j])
+        {
+            temp[k] = arr[i];
+            k++;
+            i++;
+        }
+        else
+        {
+            temp[k] = arr[j];
+            ans += mid - i + 1;
+            k++;
+            j++;
+        }
+    }
+
+    // If some elements are remaining
+    while (i <= mid)
+    {
+        temp[k++] = arr[i++];
+    }
+    while (j <= e)
+    {
+        temp[k++] = arr[j++];
+    }
+
+    // Copy the value
+    for (int i = s; i <= e; i++)
+    {
+        arr[i] = temp[i];
+    }
+
+    return ans;
+}
+
+int inversionCount(int *arr, int s, int e)
+{
+    // Base Case
+    if (s >= e)
+        return 0;
+
+    // Step 1 : Divide
+    int mid = s + (e - s) / 2;
+
+    // Step 2 : Sort
+    int x = inversionCount(arr, s, mid);
+    int y = inversionCount(arr, mid + 1, e);
+
+    // Step 3 : Merge
+    int z = mergeFunc(arr, s, e);
+
+    return x + y + z;
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    cout << inversionCount(arr, 0, n - 1);
+}
+
+// _____________________________________________________________________________________
+
+signed main(void)
+{
+    auto t1 = std::chrono::high_resolution_clock::now();
+    ayush_2574_cp();
+    solve();
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+    // cout << "Time : " << duration.count() << " ms" << endl;
+    debug(duration.count());
+    return 0;
+}
