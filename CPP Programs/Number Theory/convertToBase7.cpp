6d3b4e761c4ca29e9b323d223b0e118cd64316d1
aryan2574 <ayush.aryan94@yahoo.com>
2023-10-14 11:43:01 +0530

    convert decimal to base 7

---
+++ CPP Programs/Number Theory/convertToBase7.cpp
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1000000007
+#define mod1 998244353
+#define INF 1e18
+#define PI 3.141592653589793238462
+#define nline "\n"
+
+#define sz(x) ((int)(x).size())
+#define set_bits __builtin_popcount
+#define set_bitsll __builtin_popcountll
+#define setp(x, y) fixed << setprecision(y) << x
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#define scnfi(n) scanf("%d", &n)
+#define scnfii(n, m) scanf("%d%d", &n, &m)
+#define scnfl(n) scanf("%I64d", &n)
+#define scnfll(n, m) scanf("%lld%lld", &n, &m)
+#define scnfstr(str) scanf("%s", &str);
+
+#define prntfi(n) printf("%d\n", n)
+#define prntfii(n, m) printf("%d %d\n", n, m)
+#define prntfl(n) printf("%I64d\n", n)
+#define prntfll(n, m) printf("%I64d %I64d\n", n, m)
+#define prntfstr(s) printf("%s\n", s)
+
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+void _print(ll t)
+{
+    cerr << t;
+}
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+void ayush_2574_cp()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
+}
+
+// ____________________________________________________________________________________
+
+/*
+Given an integer num, return a string of its base 7 representation.
+
+Example 1:
+Input: num = 100
+Output: "202"
+
+Example 2:
+Input: num = -7
+Output: "-10"
+*/
+
+string converToBase7(int num)
+{
+    if (num == 0)
+        return "0";
+    int n = num > 0 ? num : -num;
+
+    string ans = "";
+    while (n > 0)
+    {
+        ans += (n % 7) + '0';
+        n = n / 7;
+    }
+    reverse(ans.begin(), ans.end());
+    return num > 0 ? ans : ("-" + ans);
+}
+
+string convertToBase7Recursion(int n)
+{
+    if (n < 0)
+        return "-" + convertToBase7Recursion(-n);
+    if (n < 7)
+        return to_string(n);
+    return convertToBase7Recursion(n / 7) + to_string(n % 7);
+}
+
+void solve()
+{
+    int num = -7;
+
+    cout << converToBase7(num) << nline;
+
+    cout << convertToBase7Recursion(num) << nline;
+}
+
+// _____________________________________________________________________________________
+
+signed main(void)
+{
+    auto t1 = std::chrono::high_resolution_clock::now();
+    ayush_2574_cp();
+    solve();
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+    // cout << "Time : " << duration.count() << " ms" << endl;
+    debug(duration.count());
+    return 0;
+}
