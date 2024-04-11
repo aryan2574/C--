062700e92e6175d117605af96b386f8d54e92110
aryan2574 <ayush.aryan94@yahoo.com>
2024-04-11 19:46:06 +0530

    uni directional and weighted graph

---
+++ CPP Programs/Graph/weighted_graph.cpp
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1000000007
+#define mod1 998244353
+#define INF 1e18
+#define PI 3.141592653589793238462
+#define nline "\n"
+
+#define sz(x) ((int)(x).size())
+#define set_bits __builtin_popcount
+#define set_bitsll __builtin_popcountll
+#define setp(x, y) fixed << setprecision(y) << x
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#define scnfi(n) scanf("%d", &n)
+#define scnfii(n, m) scanf("%d%d", &n, &m)
+#define scnfl(n) scanf("%I64d", &n)
+#define scnfll(n, m) scanf("%lld%lld", &n, &m)
+#define scnfstr(str) scanf("%s", &str);
+
+#define prntfi(n) printf("%d\n", n)
+#define prntfii(n, m) printf("%d %d\n", n, m)
+#define prntfl(n) printf("%I64d\n", n)
+#define prntfll(n, m) printf("%I64d %I64d\n", n, m)
+#define prntfstr(s) printf("%s\n", s)
+
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+void _print(ll t)
+{
+    cerr << t;
+}
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+void ayush_2574_cp()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
+}
+
+// ____________________________________________________________________________________
+// Graph Adjancy List Implementation
+class Graph
+{
+    unordered_map<string, list<pair<string, int>>> l;
+
+public:
+    void addEdge(string x, string y, bool bidir, int wt)
+    {
+        l[x].push_back(make_pair(y, wt));
+        if (bidir)
+            l[y].push_back(make_pair(x, wt));
+    }
+
+    void printAdjList()
+    {
+        // Iterate over all the keys in the map
+        for (auto p : l)
+        {
+            string city = p.first;
+            list<pair<string, int>> nbrs = p.second;
+
+            cout << city << " -> ";
+
+            for (auto nbr : nbrs)
+            {
+                string dest = nbr.first;
+                int dist = nbr.second;
+
+                cout << dest << " " << dist << " , ";
+            }
+            cout << nline;
+        }
+    }
+};
+
+void solve()
+{
+    Graph g;
+    g.addEdge("A", "B", true, 20);
+    g.addEdge("B", "D", true, 40);
+    g.addEdge("A", "C", true, 10);
+    g.addEdge("C", "D", true, 40);
+    g.addEdge("A", "D", false, 50);
+
+    g.printAdjList();
+}
+
+// _____________________________________________________________________________________
+
+signed main(void)
+{
+    auto t1 = std::chrono::high_resolution_clock::now();
+    ayush_2574_cp();
+    solve();
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+    // cout << "Time : " << duration.count() << " ms" << endl;
+    debug(duration.count());
+    return 0;
+}
