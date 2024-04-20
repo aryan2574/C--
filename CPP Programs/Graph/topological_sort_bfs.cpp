b6eb51728361be66dbbb28f70c1ba5f3e834cb62
aryan2574 <ayush.aryan94@yahoo.com>
2024-04-20 19:50:53 +0530

    Topological sort BFS

---
+++ CPP Programs/Graph/topological_sort_bfs.cpp
@@ -0,0 +1,160 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1000000007
+#define mod1 998244353
+#define INF 1e18
+#define PI 3.141592653589793238462
+#define nline "\n"
+
+#define sz(x) ((int)(x).size())
+#define set_bits __builtin_popcount
+#define set_bitsll __builtin_popcountll
+#define setp(x, y) fixed << setprecision(y) << x
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#define scnfi(n) scanf("%d", &n)
+#define scnfii(n, m) scanf("%d%d", &n, &m)
+#define scnfl(n) scanf("%I64d", &n)
+#define scnfll(n, m) scanf("%lld%lld", &n, &m)
+#define scnfstr(str) scanf("%s", &str);
+
+#define prntfi(n) printf("%d\n", n)
+#define prntfii(n, m) printf("%d %d\n", n, m)
+#define prntfl(n) printf("%I64d\n", n)
+#define prntfll(n, m) printf("%I64d %I64d\n", n, m)
+#define prntfstr(s) printf("%s\n", s)
+
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+void _print(ll t)
+{
+    cerr << t;
+}
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+void ayush_2574_cp()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
+}
+
+// ____________________________________________________________________________________
+
+class Graph
+{
+    list<int> *l;
+    int V;
+
+public:
+    Graph(int V)
+    {
+        this->V = V;
+        l = new list<int>[V];
+    }
+
+    void addEdge(int x, int y)
+    {
+        l[x].push_back(y);
+    }
+
+    void topological_sort()
+    {
+        // Create an Indegree array
+        int *indegree = new int[V];
+        // Initially mark all indegrees as 0
+        for (int i = 0; i < V; i++)
+        {
+            indegree[i] = 0;
+        }
+
+        // Update indegree by traversing edges x -> y
+        // indegree[y] ++
+
+        for (int i = 0; i < V; i++)
+        {
+            for (auto y : l[i])
+            {
+                indegree[y]++;
+            }
+        }
+
+        // BFS
+        queue<int> q;
+        // Step 1: Find nodes with 0 indegree
+        for (int i = 0; i < V; i++)
+        {
+            if (indegree[i] == 0)
+            {
+                q.push(i);
+            }
+        }
+
+        // Start removing elements from queue
+        while (!q.empty())
+        {
+            int node = q.front();
+            cout << node << " ";
+            q.pop();
+
+            // Iterate over nbrs of that node and reduce their indegree by 1
+            for (auto nbr : l[node])
+            {
+                indegree[nbr]--;
+                if (indegree[nbr] == 0)
+                {
+                    q.push(nbr);
+                }
+            }
+        }
+    }
+};
+
+void solve()
+{
+    Graph g(6);
+
+    g.addEdge(0, 2);
+    g.addEdge(1, 2);
+    g.addEdge(1, 4);
+    g.addEdge(2, 3);
+    g.addEdge(2, 5);
+    g.addEdge(3, 5);
+    g.addEdge(4, 5);
+
+    g.topological_sort();
+}
+
+// _____________________________________________________________________________________
+
+signed main(void)
+{
+    auto t1 = std::chrono::high_resolution_clock::now();
+    ayush_2574_cp();
+    solve();
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+    // cout << "Time : " << duration.count() << " ms" << endl;
+    debug(duration.count());
+    return 0;
+}
