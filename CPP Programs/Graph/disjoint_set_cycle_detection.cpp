f379f1089356ed2752ff63148e25d578a7995acb
aryan2574 <ayush.aryan94@yahoo.com>
2024-04-26 22:50:01 +0530

    disjoint set cycle detection

---
+++ CPP Programs/Graph/disjoint_set_cycle_detection.cpp
@@ -0,0 +1,146 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1000000007
+#define mod1 998244353
+#define INF 1e18
+#define PI 3.141592653589793238462
+#define nline "\n"
+
+#define sz(x) ((int)(x).size())
+#define set_bits __builtin_popcount
+#define set_bitsll __builtin_popcountll
+#define setp(x, y) fixed << setprecision(y) << x
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#define scnfi(n) scanf("%d", &n)
+#define scnfii(n, m) scanf("%d%d", &n, &m)
+#define scnfl(n) scanf("%I64d", &n)
+#define scnfll(n, m) scanf("%lld%lld", &n, &m)
+#define scnfstr(str) scanf("%s", &str);
+
+#define prntfi(n) printf("%d\n", n)
+#define prntfii(n, m) printf("%d %d\n", n, m)
+#define prntfl(n) printf("%I64d\n", n)
+#define prntfll(n, m) printf("%I64d %I64d\n", n, m)
+#define prntfstr(s) printf("%s\n", s)
+
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+void _print(ll t)
+{
+    cerr << t;
+}
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+void ayush_2574_cp()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
+}
+
+// ____________________________________________________________________________________
+
+class Graph
+{
+    vector<pair<int, int>> l; // Changed to vector
+    int V;
+
+public:
+    Graph(int V) : V(V) {}
+
+    void addEdge(int u, int v)
+    { // Changed return type to void
+        l.push_back(make_pair(u, v));
+    }
+
+    int findSet(int i, int *parent)
+    {
+        if (parent[i] == -1)
+            return i;
+        return findSet(parent[i], parent);
+    }
+
+    void union_set(int x, int y, int *parent)
+    {
+        int s1 = findSet(x, parent);
+        int s2 = findSet(y, parent);
+        if (s1 != s2)
+            parent[s2] = s1;
+    }
+
+    bool contains_cycle()
+    {
+        int *parent = new int[V];
+        for (int i = 0; i < V; i++)
+        {
+            parent[i] = -1;
+        }
+        for (auto edge : l)
+        {
+            int i = edge.first;
+            int j = edge.second;
+
+            int s1 = findSet(i, parent);
+            int s2 = findSet(j, parent);
+
+            if (s1 != s2)
+                union_set(s1, s2, parent);
+            else
+            {
+                delete[] parent;
+                return true;
+            }
+        }
+        delete[] parent; // Moved deletion of parent outside loop
+        return false;
+    }
+};
+
+void solve()
+{
+    Graph g(4);
+    g.addEdge(0, 1);
+    g.addEdge(1, 2);
+    g.addEdge(2, 3);
+    g.addEdge(3, 0);
+
+    if (g.contains_cycle())
+        cout << "Yes, Contains cycle" << nline;
+    else
+        cout << "No, does not contains cycle" << nline;
+}
+
+// _____________________________________________________________________________________
+
+signed main(void)
+{
+    auto t1 = std::chrono::high_resolution_clock::now();
+    ayush_2574_cp();
+    solve();
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+    // cout << "Time : " << duration.count() << " ms" << endl;
+    debug(duration.count());
+    return 0;
+}
