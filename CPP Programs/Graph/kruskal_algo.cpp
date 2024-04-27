964a6636104c74f1549a4cef4a8bb944798d0299
aryan2574 <ayush.aryan94@yahoo.com>
2024-04-27 17:56:24 +0530

    Kruskal Algo graph

---
+++ CPP Programs/Graph/kruskal_algo.cpp
@@ -0,0 +1,187 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1000000007
+#define mod1 998244353
+#define INF 1e18
+#define PI 3.141592653589793238462
+#define nline "\n"
+
+#define sz(x) ((int)(x).size())
+#define set_bits __builtin_popcount
+#define set_bitsll __builtin_popcountll
+#define setp(x, y) fixed << setprecision(y) << x
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#define scnfi(n) scanf("%d", &n)
+#define scnfii(n, m) scanf("%d%d", &n, &m)
+#define scnfl(n) scanf("%I64d", &n)
+#define scnfll(n, m) scanf("%lld%lld", &n, &m)
+#define scnfstr(str) scanf("%s", &str);
+
+#define prntfi(n) printf("%d\n", n)
+#define prntfii(n, m) printf("%d %d\n", n, m)
+#define prntfl(n) printf("%I64d\n", n)
+#define prntfll(n, m) printf("%I64d %I64d\n", n, m)
+#define prntfstr(s) printf("%s\n", s)
+
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+void _print(ll t)
+{
+    cerr << t;
+}
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+void ayush_2574_cp()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
+}
+
+// ____________________________________________________________________________________
+
+// DSU Data Structure
+// Path Compression + Union by rank
+
+class DSU
+{
+
+    int *parent;
+    int *rank;
+
+public:
+    DSU(int n)
+    {
+        parent = new int[n];
+        rank = new int[n];
+
+        // Parent= -1 , rank= -1
+        for (int i = 0; i < n; i++)
+        {
+            parent[i] = -1;
+            rank[i] = 1;
+        }
+    }
+
+    // Find function
+    int find(int i)
+    {
+        // Base case
+        if (parent[i] == -1)
+            return i;
+
+        return parent[i] = find(parent[i]);
+    }
+
+    // Unite (union)
+    void unite(int x, int y)
+    {
+        int s1 = find(x);
+        int s2 = find(y);
+
+        if (s1 != s2)
+        {
+            // union by rank
+            if (rank[s1] < rank[s2])
+            {
+                parent[s1] = s2;
+                rank[s2] += rank[s1];
+            }
+            else
+            {
+                parent[s2] = s1;
+                rank[s1] += rank[s2];
+            }
+        }
+    }
+};
+
+class Graph
+{
+    vector<vector<int>> edgelist;
+    int V;
+
+public:
+    Graph(int V)
+    {
+        this->V = V;
+    }
+
+    void addEdge(int x, int y, int w)
+    {
+        edgelist.push_back({w, x, y});
+    }
+
+    int kruskal_mst()
+    {
+        // Step 1 : Sort all the edges based on weight
+        sort(edgelist.begin(), edgelist.end());
+
+        int ans(0);
+
+        // Init a DSU
+        DSU s(V);
+
+        for (auto edge : edgelist)
+        {
+            int w = edge[0];
+            int x = edge[1];
+            int y = edge[2];
+
+            if (s.find(x) != s.find(y))
+            {
+                s.unite(x, y);
+                ans += w;
+            }
+        }
+        return ans;
+    }
+};
+
+void solve()
+{
+    Graph g(4);
+    g.addEdge(0, 1, 1);
+    g.addEdge(1, 3, 3);
+    g.addEdge(3, 2, 4);
+    g.addEdge(2, 0, 2);
+    g.addEdge(0, 3, 2);
+    g.addEdge(1, 2, 2);
+
+    cout << g.kruskal_mst() << nline;
+}
+
+// _____________________________________________________________________________________
+
+signed main(void)
+{
+    auto t1 = std::chrono::high_resolution_clock::now();
+    ayush_2574_cp();
+    solve();
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+    // cout << "Time : " << duration.count() << " ms" << endl;
+    debug(duration.count());
+    return 0;
+}
