6109b14a4313f1a0586aebb4a69b07339665df71
aryan2574 <ayush.aryan94@yahoo.com>
2024-05-17 18:39:18 +0530

    Min cost 2D grid DP

---
+++ CPP Programs/Dynamic Programming/min_cost-2d_grid.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1000000007
+#define mod1 998244353
+#define INF 1e18
+#define PI 3.141592653589793238462
+#define nline "\n"
+
+#define sz(x) ((int)(x).size())
+#define set_bits __builtin_popcount
+#define set_bitsll __builtin_popcountll
+#define setp(x, y) fixed << setprecision(y) << x
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#define scnfi(n) scanf("%d", &n)
+#define scnfii(n, m) scanf("%d%d", &n, &m)
+#define scnfl(n) scanf("%I64d", &n)
+#define scnfll(n, m) scanf("%lld%lld", &n, &m)
+#define scnfstr(str) scanf("%s", &str);
+
+#define prntfi(n) printf("%d\n", n)
+#define prntfii(n, m) printf("%d %d\n", n, m)
+#define prntfl(n) printf("%I64d\n", n)
+#define prntfll(n, m) printf("%I64d %I64d\n", n, m)
+#define prntfstr(s) printf("%s\n", s)
+
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+void _print(ll t)
+{
+    cerr << t;
+}
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+void ayush_2574_cp()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
+}
+
+// ____________________________________________________________________________________
+
+int dp[100][100], cost[100][100];
+
+int min_cost(int R, int C)
+{
+    for (int i = 0; i < R; i++)
+    {
+        for (int j = 0; j < C; j++)
+        {
+            // Base case
+            if (i == 0 && j == 0)
+            {
+                dp[i][j] = cost[0][0];
+            }
+            else if (i == 0)
+            {
+                dp[i][j] = dp[0][j - 1] + cost[0][j];
+            }
+            else if (j == 0)
+            {
+                dp[i][j] = dp[i - 1][0] + cost[i][0];
+            }
+            else
+            {
+                dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + cost[i][j];
+            }
+        }
+    }
+    return dp[R - 1][C - 1];
+}
+
+void solve()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+    }
+}
+
+// _____________________________________________________________________________________
+
+signed main(void)
+{
+    auto t1 = std::chrono::high_resolution_clock::now();
+    ayush_2574_cp();
+    solve();
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+    // cout << "Time : " << duration.count() << " ms" << endl;
+    debug(duration.count());
+    return 0;
+}
