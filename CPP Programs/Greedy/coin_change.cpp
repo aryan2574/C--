458d74a05f69a2144c55b4da801e831669627cd5
aryan2574 <ayush.aryan94@yahoo.com>
2024-05-18 19:48:08 +0530

    Coin change problem greedy approach

---
+++ CPP Programs/Greedy/coin_change.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1000000007
+#define mod1 998244353
+#define INF 1e18
+#define PI 3.141592653589793238462
+#define nline "\n"
+
+#define sz(x) ((int)(x).size())
+#define set_bits __builtin_popcount
+#define set_bitsll __builtin_popcountll
+#define setp(x, y) fixed << setprecision(y) << x
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#define scnfi(n) scanf("%d", &n)
+#define scnfii(n, m) scanf("%d%d", &n, &m)
+#define scnfl(n) scanf("%I64d", &n)
+#define scnfll(n, m) scanf("%lld%lld", &n, &m)
+#define scnfstr(str) scanf("%s", &str);
+
+#define prntfi(n) printf("%d\n", n)
+#define prntfii(n, m) printf("%d %d\n", n, m)
+#define prntfl(n) printf("%I64d\n", n)
+#define prntfll(n, m) printf("%I64d %I64d\n", n, m)
+#define prntfstr(s) printf("%s\n", s)
+
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+void _print(ll t)
+{
+    cerr << t;
+}
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+void ayush_2574_cp()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
+}
+
+// ____________________________________________________________________________________
+
+// Coin change problem
+
+int make_change(int *coins, int size, int money)
+{
+    sort(coins, coins + size, greater<int>());
+    int count = 0, i = 0;
+
+    while (money > 0)
+    {
+        if (i < size && money >= coins[i])
+        {
+            money -= coins[i];
+            ++count;
+        }
+        else
+        {
+            i++;
+        }
+
+        if (i == size)
+        {
+            cout << "No answer";
+            return -1; // Return -1 to indicate failure
+        }
+    }
+
+    return count;
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    int coins[] = {1, 2, 5, 10, 20, 50, 100, 200, 500, 2000};
+    int t = sizeof(coins) / sizeof(coins[0]);
+
+    int result = make_change(coins, t, n);
+    if (result != -1)
+    {
+        cout << result;
+    }
+}
+// _____________________________________________________________________________________
+
+signed main(void)
+{
+    auto t1 = std::chrono::high_resolution_clock::now();
+    ayush_2574_cp();
+    solve();
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+    // cout << "Time : " << duration.count() << " ms" << endl;
+    debug(duration.count());
+    return 0;
+}
