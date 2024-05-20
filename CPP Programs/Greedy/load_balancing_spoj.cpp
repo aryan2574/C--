38ee768368a878597e1571fc86ad04fceef6bbf8
aryan2574 <ayush.aryan94@yahoo.com>
2024-05-20 19:42:35 +0530

    SPOJ load balancing problem

---
+++ CPP Programs/Greedy/load_balancing_spoj.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define mod 1000000007
+#define mod1 998244353
+#define INF 1e18
+#define PI 3.141592653589793238462
+#define nline "\n"
+
+#define sz(x) ((int)(x).size())
+#define set_bits __builtin_popcount
+#define set_bitsll __builtin_popcountll
+#define setp(x, y) fixed << setprecision(y) << x
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#define scnfi(n) scanf("%d", &n)
+#define scnfii(n, m) scanf("%d%d", &n, &m)
+#define scnfl(n) scanf("%I64d", &n)
+#define scnfll(n, m) scanf("%lld%lld", &n, &m)
+#define scnfstr(str) scanf("%s", &str);
+
+#define prntfi(n) printf("%d\n", n)
+#define prntfii(n, m) printf("%d %d\n", n, m)
+#define prntfl(n) printf("%I64d\n", n)
+#define prntfll(n, m) printf("%I64d %I64d\n", n, m)
+#define prntfstr(s) printf("%s\n", s)
+
+#ifndef ONLINE_JUDGE
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
+#else
+#define debug(x)
+#endif
+void _print(ll t)
+{
+    cerr << t;
+}
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+void ayush_2574_cp()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+#endif
+}
+
+// ____________________________________________________________________________________
+
+// SPOJ - Load balancing
+
+void solve()
+{
+    int n;
+    while (cin >> n)
+    {
+        if (n == -1)
+        {
+            break;
+        }
+
+        vector<int> jobs(n);
+        int totalJobs = 0;
+
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> jobs[i];
+            totalJobs += jobs[i];
+        }
+
+        // Check if rebalancing is possible
+        if (totalJobs % n != 0)
+        {
+            cout << -1 << endl;
+            continue;
+        }
+
+        int target = totalJobs / n;
+        int rounds = 0;
+        int imbalance = 0;
+
+        for (int i = 0; i < n; ++i)
+        {
+            // Calculate the current imbalance at this processor
+            imbalance += jobs[i] - target;
+            // The number of rounds needed is the sum of absolute imbalances seen so far
+            rounds = max(rounds, abs(imbalance));
+        }
+
+        cout << rounds << endl;
+    }
+}
+
+// _____________________________________________________________________________________
+
+signed main(void)
+{
+    auto t1 = std::chrono::high_resolution_clock::now();
+    ayush_2574_cp();
+    solve();
+    auto t2 = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+    // cout << "Time : " << duration.count() << " ms" << endl;
+    debug(duration.count());
+    return 0;
+}
